5dd836d6fe67b47fb757f8f48b47e0c24f259f30
Alexjur5676 <alex9992142@mail.ru>
2022-08-12 05:21:13 +0700

    Add task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -8,17 +8,17 @@ Console.WriteLine("Введите число");
 string number = Console.ReadLine();
 if (number.Length > 5)
 {
-    Console.WriteLine($"Число {number} не  является пятизначным ");
+    Console.WriteLine($"Число {number} не  является пятизначным!");
 }
 else
 {
     if (number[0] == number[4] & number[1] == number[3])
     {
-        Console.WriteLine("Число является палиндромом!");
+        Console.WriteLine($"Число {number} является палиндромом");
     }
     else
     {
-        Console.WriteLine("Число не является палиндромом!");
+        Console.WriteLine($"Число {number} не является палиндромом");
     }
 }
 
